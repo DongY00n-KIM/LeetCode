38dd3975fea890f8d3ddefd136c2f6079759dc80
DongYoonKim <111625791+DongY00n-KIM@users.noreply.github.com>
2023-01-12 09:42:24 -0800

    Time: 12 ms (75.35%), Space: 14 MB (77.26%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
@@ -10,14 +10,13 @@
  */
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        if list1 == nil || list2 == nil {
-            return list1 == nil ? list2 : list1
-        }
+        if list1 == nil || list2 == nil {return list1 == nil ? list2 : list1}
+        
         if list1!.val >= list2!.val{
             list2?.next = mergeTwoLists(list1, list2?.next)
             return list2
         }
-        else {
+        else{
             list1?.next = mergeTwoLists(list1?.next, list2)
             return list1
         }
