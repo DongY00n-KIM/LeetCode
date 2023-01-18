9e447d9c78506f6c064161b149699c891cf80bea
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-01-18 10:37:02 -0800

    Time: 14 ms (59.01%), Space: 14.5 MB (14.84%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.swift
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        helperFunc(root?.left, root?.right)
+    }
+
+    func helperFunc(_ leftNode:TreeNode?, _ rightNode:TreeNode?) -> Bool {
+        if leftNode == nil && rightNode == nil{
+            return true
+        }
+        if leftNode == nil || rightNode == nil{
+            return false
+        }
+        if leftNode!.val != rightNode!.val{
+            return false
+        }
+        return helperFunc(leftNode!.left, rightNode!.right) && helperFunc(leftNode!.right, rightNode!.left)
+}
+}
