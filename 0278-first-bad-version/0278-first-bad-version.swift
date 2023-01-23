66d51283a4ae36260dce72773b596d8eb513e83c
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-01-23 12:15:20 -0800

    Time: 5 ms (37.64%), Space: 13.6 MB (59.27%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.swift
+++ 0278-first-bad-version/0278-first-bad-version.swift
@@ -7,19 +7,17 @@ class Solution : VersionControl {
     func firstBadVersion(_ n: Int) -> Int {
         var l : Int = 1
         var r : Int = n
-        var badVer : Int = n
         
-        while l <= r {
-            var m = (l + r) / 2
+        while l < r {
+            var m = l + (r - l) / 2
             
             if isBadVersion(m){
-                badVer = m
-                r = m - 1
+                r = m 
             }
             else{
                 l = m + 1
             }
         }
-        return badVer
+        return l
     }
 }
