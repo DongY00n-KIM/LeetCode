345ca09fb729ed5e205e85f1f5ada61fb917f888
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-01-23 09:35:09 -0800

    Time: 30 ms (46.20%), Space: 14.8 MB (36.71%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.swift
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.swift
@@ -17,8 +17,8 @@ class Solution {
     func maxDepth(_ root: TreeNode?) -> Int {
         guard let node = root else{return 0}
         
-        var maxHeight = 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+      //  var maxHeight = 1 + max(maxDepth(root?.left), maxDepth(root?.right))
         
-        return maxHeight
+      return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
     }
 }
