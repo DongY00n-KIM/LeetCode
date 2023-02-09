bca0c05715a2836c212e6dadd1bba8981cd534df
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-02-09 11:09:56 -0800

    Time: 134 ms (52.94%), Space: 18.8 MB (56.86%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func countPrimes(_ n: Int) -> Int {
+        if n < 3 {
+            return 0
+        }
+      var isPrime = Array(repeating: true, count: n)
+    isPrime[0] = false
+    isPrime[1] = false
+
+    for i in 2..<Int(sqrt(Double(n))) + 1 {
+        if isPrime[i] {
+            for j in stride(from: i * i, to: n, by: i) {
+                isPrime[j] = false
+            }
+        }
+    }
+
+    return isPrime.filter { $0 }.count
+}
+}
