670f9d163adf4509a4b421c4346a9d100661b1b3
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-04-11 03:51:53 -0700

    Time: 12 ms (83.18%), Space: 14.4 MB (17.14%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        for i in 1..<nums.count{
+            nums[i] = nums[i - 1] + nums[i]
+        }
+        return nums
+    }
+}
