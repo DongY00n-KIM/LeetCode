c813d4abe4f929b72874b38fa35feec7893d4830
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-10-15 10:26:31 -0400

    Time: 10 ms (84.57%), Space: 55.4 MB (51.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -9,12 +9,25 @@ class Solution {
         return false;
         */
         
+        /*
         Set duplicateSet = new HashSet<Integer>();
         for(int n : nums){
             if(duplicateSet.contains(n))
                 return true;
             else
                 duplicateSet.add(n);
+        }
+        return false;
+        */
+        Set dupSet = new HashSet<Integer>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(dupSet.contains(nums[i])){
+                return true;
+            }
+            else{
+                dupSet.add(nums[i]);
+            }
         }
         return false;
     }
