fee79e5e7b4ca9c8862925e18f91cc3a295fc40e
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-10-15 10:26:00 -0400

    Time: 11 ms (57.52%), Space: 55.1 MB (59.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        /*
         Arrays.sort(nums);
         for(int i = 1; i < nums.length; i++){
             if(nums[i - 1] == nums[i])
                 return true;
+        }
+        return false;
+        */
+        
+        Set duplicateSet = new HashSet<Integer>();
+        for(int n : nums){
+            if(duplicateSet.contains(n))
+                return true;
+            else
+                duplicateSet.add(n);
         }
         return false;
     }
