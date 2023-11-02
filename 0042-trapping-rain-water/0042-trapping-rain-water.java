63233b8123991454a1ce6e258ebd4894038201e1
DongYoon Kim (Roy) <111625791+DongyoonKim-Roy@users.noreply.github.com>
2023-11-02 08:35:38 -0400

    Time: 1 ms (75.88%), Space: 43.9 MB (87.27%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        int maxL = 0;
+        int maxR = 0;
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+        
+        while(l <= r){
+            if(height[l] <= height[r]){
+                if(height[l] >= maxL)
+                    maxL = height[l];
+                else
+                    res += maxL - height[l];
+                l++;
+            }
+            else{
+                if(height[r] >= maxR)
+                    maxR = height[r];
+                else
+                    res += maxR - height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+}
